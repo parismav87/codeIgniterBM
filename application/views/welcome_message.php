0434ac66c084525067aafabc1ec0640ca0861ea3
Paris Mavromoustakos <parismav87@gmail.com>
2015-11-05 17:26:09 +0200

    change

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -68,7 +68,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <body>
 
 <div id="container">
-	<h1>this is a test2222</h1>
+	<h1>this is nothing</h1>
 
 	<div id="body">
 		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
